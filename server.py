c62f408c2cf0311d7db2b40eba1f7b8a502ebfc4
Wery <44234831+Wery-wer@users.noreply.github.com>
2023-12-24 18:16:14 +0700

    Update server.py

--- server.py
+++ server.py
@@ -5,34 +5,63 @@ import datetime
 class HospitalServer:
     def __init__(self):
         self.waktu = {
-            'Klinik Mata' : 0,
-            'Klinik THT' : 0
+            'Klinik Mata': 0,
+            'Klinik THT': 0
         }
-        
+
         self.klinik = {
             'Klinik Mata': [],
             'Klinik THT': [],
         }
 
+        # Load existing data from file, if any
+        self.load_data()
+
+    def load_data(self):
+        try:
+            with open("hospital_data.txt", mode='r') as file:
+                for line in file:
+                    clinic_name, medical_record_number, name, birth_date, queue_number, register_time = line.strip().split('\t')
+                    self.klinik[clinic_name].append({
+                        'clinic_name': clinic_name,
+                        'medical_record_number': medical_record_number,
+                        'name': name,
+                        'birth_date': birth_date,
+                        'queue_number': int(queue_number),
+                        'register_time': register_time
+                    })
+        except FileNotFoundError:
+            pass
+
+    def save_data(self):
+        with open("hospital_data.txt", mode='w') as file:
+            for clinic_name, patients in self.klinik.items():
+                for patient in patients:
+                    file.write('\t'.join([str(patient[field]) for field in ['clinic_name', 'medical_record_number', 'name', 'birth_date', 'queue_number', 'register_time']]) + '\n')
 
     def register_pasien(self, clinic_name, medical_record_number, name, birth_date):
         if clinic_name not in self.klinik:
             return "Klinik tidak valid"
 
-
         queue_number = len(self.klinik[clinic_name]) + 1
-        self.klinik[clinic_name].append({
+        patient_data = {
+            'clinic_name': clinic_name,
             'medical_record_number': medical_record_number,
             'name': name,
             'birth_date': birth_date,
             'queue_number': queue_number,
-            'register_time': datetime.datetime.now()
-        })
+            'register_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+        self.klinik[clinic_name].append(patient_data)
 
         if queue_number == 1:
             self.waktu[clinic_name] = datetime.datetime.now()
 
-        return f"Registrasi berhasil. Nomor antrean Anda di {clinic_name}: {queue_number}",queue_number,clinic_name
+        # Save data to file after each registration
+        self.save_data()
+
+        return f"Registrasi berhasil. Nomor antrean Anda di {clinic_name}: {queue_number}", queue_number, clinic_name
+
 
 
     def waktu_estimasi(self, clinic_name, queue_number):
